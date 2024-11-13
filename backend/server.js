fa42e9356454d4f19e2998fc980b474de0a95b89
Pranav <pranav@apptodate.co.il>
2024-11-13 11:30:53 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.route.js";
+import connectToMongoDb from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  connectToMongoDb();
+  console.log(`server runnig on port ${PORT}`);
+});
